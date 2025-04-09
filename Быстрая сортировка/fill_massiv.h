3e42c186bd8ed2ba3a2729e3b6e7d9a8095baa5c
k0swel <romanleonenko32@gmail.com>
2025-04-09 22:11:10 +0300

    Быстрая сортировка(Quick sort)

---
+++ Быстрая сортировка/fill_massiv.h
@@ -0,0 +1,11 @@
+#ifndef fill_massiv
+#define fill_massiv
+
+#include <chrono>
+#include <random>
+#include <vector>
+
+std::vector<int> create_rand_massiv(int size, int min_rand_number, int max_rand_number); // прототип функции создания массива.
+
+
+#endif
